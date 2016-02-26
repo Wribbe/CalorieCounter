c356d2c3d8a05e68b1f8d0ce13e650c11af6d2ec
Stefan Eng <eng_steff@hotmail.com>
2016-02-26 06:14:37 +0100

    Change name: fab -> add_ingredient.

--- app/src/main/java/com/wribbe/android/caloriecounter/MainActivity.java
+++ app/src/main/java/com/wribbe/android/caloriecounter/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton add_ingredient = (FloatingActionButton) findViewById(R.id.add_ingredient);
+        add_ingredient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
