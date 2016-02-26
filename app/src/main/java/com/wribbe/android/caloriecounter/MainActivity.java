6496b86911eba8a314281954c345b7768ef480e5
Stefan Eng <eng_steff@hotmail.com>
2016-02-26 06:34:27 +0100

    Launch AddIngredientActivity on button press.

--- app/src/main/java/com/wribbe/android/caloriecounter/MainActivity.java
+++ app/src/main/java/com/wribbe/android/caloriecounter/MainActivity.java
@@ -1,8 +1,8 @@
 package com.wribbe.android.caloriecounter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,10 +22,14 @@ public class MainActivity extends AppCompatActivity {
         add_ingredient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                addIngredient(view);
             }
         });
+    }
+
+    public void addIngredient(View view) {
+        Intent intent = new Intent(this, AddIngredientActivity.class);
+        startActivity(intent);
     }
 
     @Override
