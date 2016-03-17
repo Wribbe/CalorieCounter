4ab85a9fb299e662db6ecd17fef9c5398cb57e43
Stefan Eng <eng_steff@hotmail.com>
2016-03-17 17:47:32 +0100

    Wire up buttons to test-toasts.

--- app/src/main/java/com/wribbe/android/caloriecounter/AddIngredientActivity.java
+++ app/src/main/java/com/wribbe/android/caloriecounter/AddIngredientActivity.java
@@ -1,9 +1,12 @@
 package com.wribbe.android.caloriecounter;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class AddIngredientActivity extends AppCompatActivity {
@@ -12,13 +15,30 @@ public class AddIngredientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingredient);
-        returnOutput();
+        //returnOutput();
+        Button button_cancel = (Button) findViewById(R.id.ingredients_button_cancel);
+        Button button_save = (Button) findViewById(R.id.ingredients_button_save);
+
+        final Context view_context = this;
+
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Utils.toast(view_context, "Pressed cancel.");
+            }
+        });
+
+        button_save.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Utils.toast(view_context, "Pressed save.");
+            }
+        });
     }
 
     @Override
     protected void onPause(){
         super.onPause();
         String ingredientName = getTextFromField(R.id.ingredient_name);
+
         if (ingredientName.equals("")) {
             System.out.println("No ingredient name.");
         } else {
