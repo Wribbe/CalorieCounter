6fcd0db4c1f4370cf59ae405a9cc299d299bb8ae
Stefan Eng <eng_steff@hotmail.com>
2016-03-19 09:05:27 +0100

    Write a failing local unit-test.

--- app/src/main/java/com/wribbe/android/caloriecounter/TestTutorial.java
+++ app/src/main/java/com/wribbe/android/caloriecounter/TestTutorial.java
@@ -4,4 +4,7 @@ package com.wribbe.android.caloriecounter;
  * Created by steff on 3/19/16.
  */
 public class TestTutorial {
+    public static boolean isValidEmail(String email) {
+        return false;
+    }
 }
--- app/src/test/java/com/wribbe/android/caloriecounter/TestTutorialTest.java
+++ app/src/test/java/com/wribbe/android/caloriecounter/TestTutorialTest.java
@@ -1,7 +1,20 @@
 package com.wribbe.android.caloriecounter;
 
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
 /**
  * Created by steff on 3/19/16.
  */
 public class TestTutorialTest {
+
+    @Test
+    public void TestTutorial_ReturnsTrue() {
+        assertThat(TestTutorial.isValidEmail("name@email.com"), is(true));
+    }
+
 }
