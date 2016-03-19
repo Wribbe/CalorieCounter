22f82b6c42eb5a486969a661c4ebfb44c16eb375
Stefan Eng <eng_steff@hotmail.com>
2016-03-19 09:46:38 +0100

    Mock context string and fail test.

--- app/src/test/java/com/wribbe/android/caloriecounter/TestTutorialTest.java
+++ app/src/test/java/com/wribbe/android/caloriecounter/TestTutorialTest.java
@@ -1,14 +1,17 @@
 package com.wribbe.android.caloriecounter;
 
+import android.content.Context;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.regex.Pattern;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
 
 /**
  * Created by steff on 3/19/16.
@@ -19,5 +22,22 @@ public class TestTutorialTest {
     @Test
     public void TestTutorial_ReturnsTrue() {
         assertThat(TestTutorial.isValidEmail("name@email.com"), is(true));
+    }
+
+    private static final String FAKE_STRING = "HELLO WORLD";
+
+    @Mock
+    Context mMockContext;
+
+    @Test
+    public void readStringFromContext_LoacalizedString() {
+        // Given a mocked Context injected into the object under test...
+        when(mMockContext.getString(R.string.ingredient_flag_name)).thenReturn(FAKE_STRING);
+
+        TestTutorial myObjectUnderTest = new TestTutorial(mMockContext);
+
+        String result = myObjectUnderTest.getIngredientFlagName();
+
+        assertThat(result, is(FAKE_STRING+'a'));
     }
 }
