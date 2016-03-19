3839d8a43fc0ee5c731980f136556ceaecff5e20
Stefan Eng <eng_steff@hotmail.com>
2016-03-19 09:21:59 +0100

    Previous test runs fine in MockitoRunnner.

--- app/src/test/java/com/wribbe/android/caloriecounter/TestTutorialTest.java
+++ app/src/test/java/com/wribbe/android/caloriecounter/TestTutorialTest.java
@@ -1,20 +1,23 @@
 package com.wribbe.android.caloriecounter;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import java.util.regex.Pattern;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Created by steff on 3/19/16.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TestTutorialTest {
 
     @Test
     public void TestTutorial_ReturnsTrue() {
         assertThat(TestTutorial.isValidEmail("name@email.com"), is(true));
     }
-
 }
